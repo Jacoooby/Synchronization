88d8afa16eea5ad8c3b629d52cd302fc7ff5962d
Jacoooby <jacobberard914@gmail.com>
2026-02-19 16:48:27 -0600

    asks user for inputs and created DiningPhilosopher class.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+class DiningPhilosophers implements Runnable {
+    private final int philosophers;
+    private final int meals;
+
+    DiningPhilosophers(int philosophers, int meals) {
+        this.philosophers = philosophers;
+        this.meals = meals;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of philosophers P: ");
+        int P = scanner.nextInt();
+
+        System.out.print("Enter the number of total meals to be eaten M: ");
+        int M = scanner.nextInt();
+
+        // array to store forks for number of philosophers
+        // do later
+        // ....
+
+
+        // array to store threads for philosophers
+        Thread[] threads = new Thread[P];
+
+        // fork a single thread for each philosopher
+        for (int i = 0; i < P; i++) {
+            threads[i] = new Thread(new DiningPhilosophers(i, M));
+            threads[i].start();
+        }
 
     }
 }
